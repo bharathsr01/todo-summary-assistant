110484463f7e68a2f0953015b96b557438dc8bb3
Bharath S R <bharathsrbharath25@gmail.com>
2025-05-22 10:34:25 +0530

    Delete slack.js

--- slack.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-async function sendToSlack(message) {
-  await axios.post(process.env.SLACK_WEBHOOK_URL, {
-    text: message,
-  });
-}
-
-module.exports = { sendToSlack };
